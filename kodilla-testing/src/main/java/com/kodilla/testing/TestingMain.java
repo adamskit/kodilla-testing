64cebb01a9cc9481c1bc072d7f3350bea8a03665
Tomasz Adamski <adamskit@tlen.pl>
2023-05-08 16:20:05 +0200

    Testing Add and Subtract

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main (String [] args){
-        System.out.println("Wprowadzenie do testowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int c = Calculator.Add(5, 6);
+
+        if (c == 11) {
+            System.out.println("test się udał");
+        }
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public static int Add (int a, int b){
+        return a + b;
+    }
+
+    public static int Subtract(int a, int b){
+        return a - b;
+    }
+
+}
